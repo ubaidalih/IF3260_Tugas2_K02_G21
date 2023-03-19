a1bd34962569e5a16960ea929c5c7f7427ee6728
kentliu <kent.delatinos@gmail.com>
2023-03-19 22:00:19 +0700

    style: formatting

--- src/scripts/parse.js
+++ src/scripts/parse.js
@@ -1,34 +1,37 @@
-export function parseObject(file){
-    var model = {
-        vertices : [],
-        indices  : []
-    };
-    //parse json file
-    var request = new XMLHttpRequest();
-    request.open("GET", file, false);
-    request.send(null)
-    var json = JSON.parse(request.responseText);
-    model.vertices = json.vertices;
-    var surfaces = json.surfaces;
-    for(var i = 0; i < surfaces.length; i++){
-        var surface = surfaces[i];
-        //split on -
-        var indices = surface.split("-");
-        for(var j = 1; j+1 < indices.length; j++){
-            model.indices.push(indices[0]);
-            model.indices.push(indices[j]);
-            model.indices.push(indices[j+1]);
-        }
+export function parseObject(file) {
+  const model = {
+    vertices: [],
+    indices: [],
+  };
+
+  const request = new XMLHttpRequest();
+  request.open("GET", file, false);
+  request.send();
+  const json = JSON.parse(request.responseText);
+  model.vertices = json.vertices;
+  const surfaces = json.surfaces;
+  for (let i = 0; i < surfaces.length; i++) {
+    const surface = surfaces[i];
+    const indices = surface.split("-");
+    for (let j = 1; j + 1 < indices.length; j++) {
+      model.indices.push(indices[0]);
+      model.indices.push(indices[j]);
+      model.indices.push(indices[j + 1]);
     }
-    var max   = model.vertices.reduce((a, b) => {return Math.max(a, b);});
-    var min   = model.vertices.reduce((a, b) => {return Math.min(a, b);});
-    var range = max - min;
+  }
+  const max = model.vertices.reduce((a, b) => {
+    return Math.max(a, b);
+  });
+  const min = model.vertices.reduce((a, b) => {
+    return Math.min(a, b);
+  });
+  const range = max - min;
 
-    model.vertices.forEach((item, i) => {
-        var normed = (item - min) / range;
-        model.vertices[i] = normed - 0.5;
-    });
+  model.vertices.forEach((item, i) => {
+    const normed = (item - min) / range;
+    model.vertices[i] = normed - 0.5;
+  });
 
-    console.log(model);
-    return model;
+  console.log(model);
+  return model;
 }
--- src/scripts/transformation.js
+++ src/scripts/transformation.js
@@ -1,40 +1,48 @@
-export function translationMatrix(translationFactor){
+function translationMatrix(translationFactor) {
+  return [
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    translationFactor[0], translationFactor[1], translationFactor[2], 1
+  ]
+}
+function xRotationMatrix(rotationFactor) {
+  return [
+    1, 0, 0, 0,
+    0, Math.cos(rotationFactor), -Math.sin(rotationFactor), 0,
+    0, Math.sin(rotationFactor), Math.cos(rotationFactor), 0,
+    0, 0, 0, 1
+  ]
+}
+function yRotationMatrix(rotationFactor) {
     return [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        translationFactor[0], translationFactor[1], translationFactor[2], 1
+      Math.cos(rotationFactor), 0, Math.sin(rotationFactor), 0,
+      0, 1, 0, 0,
+      -Math.sin(rotationFactor), 0, Math.cos(rotationFactor), 0,
+      0, 0, 0, 1
     ]
 }
-export function xRotationMatrix(rotationFactor){
-    return [
-        1, 0, 0, 0,
-        0, Math.cos(rotationFactor), -Math.sin(rotationFactor), 0,
-        0, Math.sin(rotationFactor), Math.cos(rotationFactor), 0,
-        0, 0, 0, 1
-    ]
+function zRotationMatrix(rotationFactor) {
+  return [
+    Math.cos(rotationFactor), -Math.sin(rotationFactor), 0, 0,
+    Math.sin(rotationFactor), Math.cos(rotationFactor), 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1
+  ]
 }
-export function yRotationMatrix(rotationFactor){
-    return [
-        Math.cos(rotationFactor), 0, Math.sin(rotationFactor), 0,
-        0, 1, 0, 0,
-        -Math.sin(rotationFactor), 0, Math.cos(rotationFactor), 0,
-        0, 0, 0, 1
-    ]
+function scaleMatrix(scaleFactor) {
+  return [
+    scaleFactor[0], 0, 0, 0,
+    0, scaleFactor[1], 0, 0,
+    0, 0, scaleFactor[2], 0,
+    0, 0, 0, 1
+  ]
 }
-export function zRotationMatrix(rotationFactor){
-    return [
-        Math.cos(rotationFactor), -Math.sin(rotationFactor), 0, 0,
-        Math.sin(rotationFactor), Math.cos(rotationFactor), 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1
-    ]
-}
-export function scaleMatrix(scaleFactor){
-    return [
-        scaleFactor[0], 0, 0, 0,
-        0, scaleFactor[1], 0, 0,
-        0, 0, scaleFactor[2], 0,
-        0, 0, 0, 1
-    ]
+
+export {
+  translationMatrix,
+  xRotationMatrix,
+  yRotationMatrix,
+  zRotationMatrix,
+  scaleMatrix
 }
